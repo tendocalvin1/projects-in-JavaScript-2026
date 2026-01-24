656b045a1d5b98e18206513fbc3b35b5e8be7602
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-01-24 16:16:07 +0300

    Basics of JavaScript that is Variables

---
+++ JavaScript/variables.js
@@ -0,0 +1,23 @@
+// basics of JavaScript 
+// variables in JavaScript
+
+var age = 20;
+let name = "Tendo"
+const country = "Uganda"
+
+console.log(age);
+console.log(name);
+console.log(country);
+
+let a = "hello";
+let b = 10;
+let c = true
+let d = null;
+let e
+
+
+console.log(typeof(a))
+console.log(typeof (b))
+console.log(typeof (c))
+console.log(typeof (d))
+console.log(typeof (e))
