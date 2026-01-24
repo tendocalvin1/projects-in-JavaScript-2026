181fdbcb9aed5a33f619726be03dcdebecbc22b4
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-01-24 16:37:34 +0300

    Operators and Type Coercion in JavaScript

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -21,3 +21,12 @@ console.log(typeof (b))
 console.log(typeof (c))
 console.log(typeof (d))
 console.log(typeof (e))
+
+
+// Operators & Type Coercion
+console.log("5" + 2); // "52" (string concatenation)
+console.log("5" - 2); // 3 (subtraction)
+console.log("5" * 2); // 10 (multiply)
+console.log("5" / 2); // 2.5(divide)
+console.log("5" > 2); // true (comparison)
+console.log("5" == 5); // true (loose equality)
